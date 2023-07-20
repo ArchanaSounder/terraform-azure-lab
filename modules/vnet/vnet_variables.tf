98b73f72af170a22ff74dc443094ae02b554891f
ArchanaSounder <81344540+ArchanaSounder@users.noreply.github.com>
2023-07-20 13:31:16 +0530

    Add files via upload

---
+++ modules/vnet/vnet_variables.tf
@@ -0,0 +1,31 @@
+# Variable Declaration for Resource group name
+variable resource_group_name {
+}
+
+# Variable Declaration for Region
+variable region {
+  }
+
+# Variable Declaration for Virtual Network Enable
+variable vnet_enable {
+  default = 1
+}
+
+# Variable Declaration for Virtual Network Name
+variable vnet_name {
+type = list
+}
+
+# Variable Declaration for Virtual Network Address
+variable vnet_address {
+    type = list
+}
+
+# Variable Declaration for Tag Value
+variable tagvalue {
+  type = map
+}
+
+# Variable Declaration for Environment
+variable environment {
+}
